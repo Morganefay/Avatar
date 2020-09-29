a6d011d8bec64bb95d6595dc6b7f4b5d43c3c272
Morganefay <marie.collin.voirin@gmail.com>
2020-09-29 15:36:59 +0200

    asymetrie des couleurs

--- main.php
+++ main.php
@@ -2,9 +2,9 @@
 
 require "vendor/autoload.php";
 
-$size= 5;
+$size= 4;
 $bigTab = [];//on instancie un tableau vide
-$colors = ['green','pink','purple','red','yellow'];
+$colors = ['green','pink','purple','red','yellow','cyan','orange','brown'];
 
 //on boucle sur le grand tableau
 for ($i=0; $i<$size ; $i++) { 
@@ -12,17 +12,20 @@ for ($i=0; $i<$size ; $i++) {
     //on boucle sur les lignes
 
     for ($j=0; $j<$size; $j++) { 
-     $color = array_rand($colors, 1);
-     $row[]= ($colors[$color]);
-
-    // $jMax = $row[count($row)-1];
-     //echo $jMax . " ";
+     
+    //on boucle sur les cases
+     for ($h=0; $h < ($size/2) ; $h++) { 
+        $color = array_rand($colors, 1);
+        $row[$h] = ($colors[$color]);
+        $row[($size - 1) - $h] = ($colors[$color]);
+     }
     };
- 
+  ksort($row);
   $bigTab[]= $row;
   
 }
 dump($bigTab);
+
 //dump($color);
 
 
